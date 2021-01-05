eab4365c15c96f693de7e906a0630aab2a6d3d12
Mike <michaelvolk1979@gmail.com>
2021-01-05 16:01:47 -0500

    small changes to test

--- testing/test_fit_resonance.m
+++ testing/test_fit_resonance.m
@@ -1,7 +1,7 @@
 expData = load('D:\data\mike\NRM\run_00000.mat');
 %% create data like fit_resonance
 freq = expData.freqList(1:expData.numFreqs) / 1E9;   %everything is in GHz
-n = 1; binSize = 2;
+n = 1; binSize = 4;
 dataStack = expData.(sprintf('imgStack%i',n));
 % data preparation
 % X/Y of unbinned data
@@ -50,8 +50,6 @@ for y = 1:length(freq)
     binDataNorm(:,:,y) = binData(:,:,y) ./ NormalizationFactor;
 end
 
-fprintf('<>   %i: starting parameter estimation\n', n);
-
 % global spectra subtraction
 binDataNorm = correct_global(binDataNorm, 0.5);
 
@@ -66,11 +64,17 @@ gpudata = transpose(gpudata); %transpose to make it 51 x pixels
 gpudata = single(gpudata);
 xValues = single(freq');
 %%
+disp('GLOBAL')
 [fitOld, guessOld, badPixelsOld] = fit_resonance(expData, binSize, freq, n, 'type',0);
+%%
+disp('LOCAL WITH GLOBAL SUBSTITUTION')
 [fitOldGlob, guessOldGlob, badPixelsOldGlob] = fit_resonance(expData, binSize, freq, n, 'type',1);
+%%
+disp('LOCAL WITH GAUSSIAN SUBSTITUTION')
 [fitOldGaus, guessOldGaus, badPixelsOldGaus] = fit_resonance(expData, binSize, freq, n, 'type',1, 'gaussianFit',1);
-[fitNew, guessNew, badPixelsNew] = fit_resonance(expData, 1, freq, n, 'type',2);
-
+%%
+disp('LOCAL GAUSSIAN')
+[fitNew, guessNew, preParamsNew] = fit_resonance(expData, binSize, freq, n, 'type',2);
 %%
 test(fitNew, fitOld, gpudata, binDataNorm, freq, binSize)
 %%
@@ -83,62 +87,69 @@ function test(fitNew, fitOld, gpudata, binDataNorm, freq, binSize)
     x = 273;
     y = 211;
     figure
-    set(gcf,'position',[150,150,1500,800])
+    set(gcf,'position',[150,150,1000,800])
 
     ax1 = subplot(3,2,1);
-    title('OLD')
-    old = filter_hot_pixels(fitOld.resonance, 'cutOff', 20,'chi', fitOld.chiSquares, 'winSize',nan);
+    old = fitOld.resonance;
+%     old = filter_hot_pixels(old, 'cutOff', 20,'chi', fitOld.chiSquares, 'winSize',nan);
 %     old = filter_hot_pixels(old, 'winSize',nan);
-    imagesc(old,'CDataMapping','scaled');
+    imagesc(old,'Parent', ax1,'CDataMapping','scaled','hittest', 'off')
+    title(ax1, 'OLD')
+
     axis equal, axis tight, axis xy
     ax1.CLim = [2.83 2.85];
 
     colorbar()
 
     ax2 = subplot(3,2,2);
-    title('New')
-    new = filter_hot_pixels(fitNew.resonance, 'cutOff', 20,'chi', fitNew.chiSquares, 'winSize',nan);
+    new = fitNew.resonance;
+%     new = filter_hot_pixels(new, 'cutOff', 20,'chi', fitNew.chiSquares, 'winSize',nan);
 %     new = filter_hot_pixels(new, 'winSize',nan);
-    imagesc(new,'Parent',ax2,'CDataMapping','scaled')
+    imagesc(new,'Parent',ax2,'CDataMapping','scaled','hittest', 'off')
     axis equal, axis tight, axis xy
+    title('New')
     ax2.CLim = [2.83 2.85];
     colorbar()
 
     ax3 = subplot(3,2,3);
-    title('X2')
     chimax = double(max([max(fitOld.chiSquares,[],'all'), max(fitNew.chiSquares,[],'all')]));
-    imagesc(fitOld.chiSquares,'Parent',ax3,'CDataMapping','scaled')
+    im = imagesc(fitOld.chiSquares,'Parent',ax3,'CDataMapping','scaled','hittest', 'off');
     axis equal, axis tight, axis xy
-%     ax3.CLim = [0 chimax];
+    title('X^2')
+    ax3.CLim = [0 chimax]; 
+    set(ax3,'ColorScale','log')
     colorbar()
-    
+
     ax4 = subplot(3,2,4);
-    title('X2')
-    imagesc(fitNew.chiSquares,'Parent',ax4,'CDataMapping','scaled')
+    imagesc(fitNew.chiSquares,'Parent',ax4,'CDataMapping','scaled','hittest', 'off')
     axis equal, axis tight, axis xy
-%     ax4.CLim = [0 chimax];
+    ax4.CLim = [0 chimax]; 
+    title('X^2')
+    set(ax4,'ColorScale','log')
+
     colorbar()
     % linkaxes([ax1 ax2 ax3 ax4])
 
     n = 0;
     points = [0 0 0 0 0];
 
-    % set(ax1,'ButtonDownFcn',@clickFitFcn)
-    set(ax1,'ButtonDownFcn',@clickFitFcn)
-
-
     ax5 = subplot(3,2,5);
-    imagesc(fitNew.resonance-fitOld.resonance,'Parent',ax5,'CDataMapping','scaled')
+    imagesc(fitNew.resonance-fitOld.resonance,'Parent',ax5,'CDataMapping','scaled','hittest', 'off')
     mx = max(abs(fitNew.resonance-fitOld.resonance),[],'all');
     set(ax5,'ColorScale','log')
+    
     colorbar()
     axis equal, axis tight, axis xy
     
     
     ax6 = subplot(3,2,6);
-    set(gcf,'WindowButtonDownFcn',@clickFitFcn)
+%     set(gcf,'WindowButtonDownFcn',@clickFitFcn)
     linkaxes([ax1 ax2 ax3 ax4 ax5]);
+    for ax = [ax1 ax2 ax3 ax4 ax5]
+       set(ax,'ButtonDownFcn',@clickFitFcn)
+    end
 
+    drawnow;
     function clickFitFcn(hObj, event)
         % Get click coordinate
         click = event.IntersectionPoint;
@@ -156,11 +167,11 @@ function test(fitNew, fitOld, gpudata, binDataNorm, freq, binSize)
         idx = xy2index(x, y,rows);
         plot(freq, 1+model_GPU(fitOld.p(:,idx), freq), 'r--','DisplayName','OLD fit')
         plot(freq, 1+model_GPU(fitNew.p(:,idx), freq), 'b','DisplayName','NewFit')
-        plot(freq, 1+model_GPU(fitOld.g(:,idx), freq), 'r:','DisplayName','')
-        plot(freq, 1+model_GPU(fitNew.g(:,idx), freq), 'b:','DisplayName','')
+        plot(freq, 1+model_GPU(fitOld.g(:,idx), freq), 'r:','DisplayName','OLD IG')
+        plot(freq, 1+model_GPU(fitNew.g(:,idx), freq), 'b:','DisplayName','New IG')
         ylabel('Intensity')
         xlabel('f (Hz)')
-        legend()
+        legend('Location','bestoutside')
         for i = 1:5
             point = points(i);
             if point ~= 0
@@ -171,7 +182,7 @@ function test(fitNew, fitOld, gpudata, binDataNorm, freq, binSize)
             point = scatter(round(x),round(y),'xr');
             points(i) = point;
         end
-
+        title(ax6, titleTxt)
         fprintf('%s OLD: %.5f, %.2e\t NEW: %.5f, %.2e\n', titleTxt, ...
             fitOld.resonance(y,x), fitOld.chiSquares(y,x), fitNew.resonance(y,x), fitNew.chiSquares(y,x))
     end
