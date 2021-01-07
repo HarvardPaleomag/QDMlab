f55b14d2334ee75aaa1bd48d1b97ef9368d40249
Mike Volk <michaelvolk1979@gmail.com>
2021-01-07 18:40:13 -0500

    adds filter and nROI plotting

--- plotting/QDM_figure.m
+++ plotting/QDM_figure.m
@@ -6,20 +6,29 @@ arguments
     data
     kwargs.figure = 'none';
     kwargs.ax = 'none';
+    kwargs.nROI = 'none';
+    kwargs.filter_hot_pixels = 0
 end
 
 if kwargs.figure == 'none'
-    map_figure = figure('Name','QDM map','units','normalized','outerposition',[0 0 1 1]);
+    map_figure = figure('Name','QDM map','units','normalized','outerposition',[0.2 0.2 0.6 0.6]);
 else
     map_figure = kwargs.figure;
 end
 
-
 if kwargs.ax == 'none'
-    ax = axes('Parent',kwargs.figure);
+    ax = axes('Parent',map_figure);
 else
     ax = kwargs.ax;
 end
+
+if kwargs.filter_hot_pixels
+    data = filter_hot_pixels(data, 'cutOff', kwargs.filter_hot_pixels, 'winSize', nan);
+end
+
+%%
+
+med = nanmedian(data,'all'); st = nanstd(data,[],'all'); mx = max(abs(data), [], 'all');
 
 % Create axes
 axis off
@@ -37,11 +46,14 @@ title({'QDM DATA'});
 box(ax,'on');
 axis(ax,'tight');
 
-mx = max(abs(data), [], 'all');
 
 % Set the remaining axes properties
-set(ax,'CLim',[-1 1] * mx/10);
+set(ax,'CLim',[-1 1] * (med + 4*st));
 axis equal, axis tight, axis xy
+
+if iscell(kwargs.nROI)
+    add_ROI(kwargs.nROI, 'ax', ax)
+end
 
 % Create colorbar
 colorbar(ax);
