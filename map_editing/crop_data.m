9b5920db97f963d906f04692cabcf853abddce6c
Mike Volk <michaelvolk1979@gmail.com>
2021-01-08 10:22:20 -0500

    adds crop_data.m UpCont.m from old repo

---
+++ map_editing/crop_data.m
@@ -0,0 +1,19 @@
+function croppedData = crop_data(data, mask)
+%{
+Helper function to crop data according to a specified mask
+
+Parameters
+----------
+    positional
+    ----------
+    data: array
+        the data
+    mask: logical-array
+        the mask consisting of 0,1
+%}
+    [x, y, w, h] = get_mask_extent(mask);
+
+    % cut around data
+    disp('<>   cropping data')
+    croppedData = data(y:y+h, x:x+w);
+end
