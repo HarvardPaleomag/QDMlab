849606f228365573487a96012cb94ad6342ffeec
Michael Volk <michaelvolk1979@gmail.com>
2021-02-22 11:49:00 -0800

    fixes issue in validators

--- validators/foldersMustExist.m
+++ validators/foldersMustExist.m
@@ -2,9 +2,8 @@
 function foldersMustExist(folders)
     % Test for equal size
     folders = correct_cell_shape(folders);
-    if ~all(cellfun(@exist, folders))
-        eid = 'function:inputError';
-        msg = sprintf('Some folders do not exist! Check cell\n');
-        throwAsCaller(MException(eid,msg))
+    for f = folders
+        f = f{:};
+        folderMustExist(f);
     end
 end
