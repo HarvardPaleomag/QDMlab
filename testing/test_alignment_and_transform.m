5761806b900dbb770706cb4a34192d03a027d1d2
Michael Volk <michaelvolk1979@gmail.com>
2021-02-22 15:40:42 -0800

    adds test file

---
+++ testing/test_alignment_and_transform.m
@@ -0,0 +1,44 @@
+a = 'D:\data\mike\NRM';
+b = 'D:\data\mike\NRM';
+%%
+ima = imread(fullfile(a, 'LED.jpg'));
+imb = imread(fullfile(b, 'LED.jpg'));
+%%
+[transForm, refFrame] = get_image_tform2(ima, imb,...
+    'checkPlot', true, 'title', 'alignment');
+%% bin image B by 4
+imb_ = imresize(imb, 1/4, 'method', 'box');
+%%
+tformdata = tform_data(imb_, transForm, refFrame);
+%%
+figure
+subplot(2,2,1)
+imagesc(ima)
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,2)
+imagesc(imresize(ima, 1/4, 'method', 'box'))
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,3)
+imagesc(imb)
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,4)
+imagesc(tformdata)
+axis 'equal'; axis xy; axis tight;
+%%
+[transForm_, refFrame_] = tform_bin_down(transForm, refFrame, 4);
+tformdata_ = tform_data(imb_, transForm_, refFrame_);
+%%
+close all
+figure
+subplot(2,2,1)
+imagesc(ima)
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,2)
+imagesc(imresize(ima, 1/4, 'method', 'box'))
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,3)
+imagesc(imb)
+axis 'equal'; axis xy; axis tight;
+subplot(2,2,4)
+imagesc(tformdata_)
+axis 'equal'; axis xy; axis tight;
