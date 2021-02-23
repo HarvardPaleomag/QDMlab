cf1bc4077cb94b73217b3409e2e99b2aee96d348
Mike Volk <michaelvolk1979@gmail.com>
2021-02-22 20:17:01 -0500

    reshape_QDM_data now uses QDMreshape

--- utilities/reshape_QDM_data.m
+++ utilities/reshape_QDM_data.m
@@ -1,9 +1,6 @@
 function data = reshape_QDM_data(data)
 % reshapes data to freq:row:col
-    data.imgStack1 = reshape(data.imgStack1, [], expData.imgNumCols, expData.imgNumRows); % reshape into freq x col x rows
-    data = permute(data.imgStack1,[3 2 1]); % permute the axis to rows x cols x freq
-
-	data.imgStack2 = reshape(data.imgStack2, [], expData.imgNumCols, expData.imgNumRows); % reshape into freq x col x rows
-    data.imgStack2 = permute(data.imgStack2,[3 2 1]); % permute the axis to rows x cols x freq
+    data.imgStack1 = QDMreshape(data.imgStack1, expData.imgNumCols, expData.imgNumRows); % reshape into freq x col x rows
+	data.imgStack2 = QDMreshape(data.imgStack2, expData.imgNumCols, expData.imgNumRows); % reshape into freq x col x rows
 end
 
