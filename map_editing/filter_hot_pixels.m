78d251f1aa4a8484bd8f320517432fdb9273ebc9
Mike Volk <michaelvolk1979@gmail.com>
2021-03-09 18:15:53 -0500

    indents logging, adds comments

--- map_editing/filter_hot_pixels.m
+++ map_editing/filter_hot_pixels.m
@@ -5,13 +5,8 @@ function filteredData = filter_hot_pixels(data, kwargs)
 % 
 % Parameters
 % ----------
-%     positional
-%     ----------
 %     data:
 %         data matrix to be filtered
-%     
-%     optional parameters
-%     -------------------
 %     cutOff: 
 %         how many standard deviations have to be exceeded for the pixel to
 %         be filtered.
@@ -55,9 +50,9 @@ data(aboveStd) = nan;
 
 %% chose mode for hot pixel calculation
 if ~strcmp(cutOff, 'none')
-    fprintf('<>   FILTER: data where data/chi is %i standard deviations above the median\n', cutOff)
+    fprintf('<>     FILTER: data where data/chi is %i standard deviations above the median\n', cutOff)
 
-    if chi ~= 0
+    if all(size(chi) == dshape)
         % pefilter chi values to catch extreme outlier
         chiFilter = abs(chi) > nanmean(chi, 'all') + 15 * nanstd(chi, 0, 'all');
         chi(chiFilter) = nan;
@@ -154,9 +149,9 @@ end
 n_pixels = sum(sum(filteredPixels));
 
 if strcmp(cutOff, 'none')
-    fprintf('<>   FILTER: B > +- 5G: removed %i / %i pixel = %.2f%%\n', n_pixels, numel(filteredPixels), n_pixels/numel(filteredPixels)*100)
+    fprintf('<>     FILTER: B > +- 5G: removed %i / %i pixel = %.2f%%\n', n_pixels, numel(filteredPixels), n_pixels/numel(filteredPixels)*100)
 else
-    fprintf('<>   FILTER: by %i stdev: removed %i / %i pixel = %.2f%% | median = %.2e, std = %.2e\n', cutOff, n_pixels, numel(filteredPixels), n_pixels/numel(filteredPixels)*100, dMed, dStd)
+    fprintf('<>     FILTER: by %i stdev: removed %i / %i pixel = %.2f%% | median = %.2e, std = %.2e\n', cutOff, n_pixels, numel(filteredPixels), n_pixels/numel(filteredPixels)*100, dMed, dStd)
 end
 
 %% checkplot
