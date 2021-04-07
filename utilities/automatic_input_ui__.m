6c7bc24b27d1d53b55786a887d79c5ee604df03f
Mike <michaelvolk1979@gmail.com>
2021-04-07 13:41:51 +0200

    fixes strcmp bug and return values

--- utilities/automatic_input_ui__.m
+++ utilities/automatic_input_ui__.m
@@ -8,12 +8,12 @@ arguments
     kwargs.MultiSelect = 'off';
 end
 
-if nFolders == 'None'
+if nFolders == 'none'
 	nFolders = {};
 
     if strcmp(kwargs.type, 'dir')
         
-        if kwargs.MultiSelect == 'on'
+        if strcmp(kwargs.MultiSelect,'on')
             path = 1;
             while path ~= 0
                 path = uigetdir('title', [kwargs.title ' cancel to stop selecting']);
@@ -22,21 +22,23 @@ if nFolders == 'None'
                 end
             end
         else
-           path = uigetdir('title', kwargs.title);
+           nFolders = {uigetdir('title', kwargs.title)};
         end
         
     end
     if strcmp(kwargs.type, 'file')
         [file,path] = uigetfile('title', kwargs.title, 'MultiSelect', kwargs.MultiSelect);
-        if kwargs.MultiSelect == 'on'
+        if strcmp(kwargs.MultiSelect,'on')
             for f = file
                 f = fullfile(path, f);
                 nFolders{end+1} = f{:};
             end
-
+        else
+            nFolders = {fullfile(path, file)};
         end
     end
 end
+
 nFolders = correct_cell_shape(nFolders);
 end
 
