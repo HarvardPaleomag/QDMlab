7805e363f58135f7d4d45c10a0a447025f2e0917
Mike <michaelvolk1979@gmail.com>
2021-04-09 13:19:13 +0200

    changed back to use UpCont

--- Bz_and_UpCont/upward_continue.m
+++ Bz_and_UpCont/upward_continue.m
@@ -48,7 +48,7 @@ if size(UC, 2) ~= 0
         if upDist == 0
             [b_uc] = Btrunc;
         else
-            [b_uc] = calculate_UC(Btrunc, upDist, 1/step); %upward continue Bz map
+            [b_uc] = UpCont(Btrunc, upDist, 1/step); %upward continue Bz map
         end
 
         [by, bx] = MITBxByFromBz(b_uc, 1/step);
@@ -105,66 +105,5 @@ else
     else
         Btrunc = Bdata(1:end, 1:end);
     end
-end
-end
-
-function [Bout] = calculate_UC(data, dh, fs)
-% Parameters
-% ----------
-%       data: double
-%           component map
-%       dh: double
-%           distance to upward continue the field map
-%       fs: double
-%           Sampling frequency in 1/m
-
-EXPAND = 1;
-% Algorithm refines sampling in the frequency domain by
-% this factor (i.e., zero padding). Increase value (should be
-% an even number) if the number of points in Bz map is small.
-
-FOVERSAMPL = 2;
-
-if EXPAND
-    Bino = data;
-
-    data = [zeros(size(data)), zeros(size(data)), zeros(size(data)); ...
-        zeros(size(data)), data, zeros(size(data)); ...
-        zeros(size(data)), zeros(size(data)), zeros(size(data))];
-end
-
-[sizeX, sizeY] = size(data');
-Nx = sizeX * FOVERSAMPL;
-Ny = sizeY * FOVERSAMPL;
-
-% Adjust sampling frequency accordingly
-% fs= fs * FOVERSAMPL;
-
-% These freq. coordinates match MATLAB's fft algorithm
-f2 = [0:Ny / 2, -(Ny / 2 - 1):-1] * fs / Ny;
-f1 = [0:Nx / 2, -(Nx / 2 - 1):-1] * fs / Nx;
-
-% Generate spatial frequency grid. A tiny 'epsilon' is added to
-% each freq. variable so as to avoid division by zero warning.
-[F2, F1] = meshgrid(f2, f1);
-
-kx = 2 * pi * F1;
-ky = 2 * pi * F2;
-k = sqrt(kx.^2+ky.^2);
-
-% Calculate the filter frequency response associated with the x component
-et = exp(-dh*k);
-
-% Compute FFT of the field map
-e = fft2(data', Nx, Ny);
-
-% Calculate x component
-Bout = ifft2(e.*et, Nx, Ny, 'symmetric')';
-
-% Crop matrices to get rid of zero padding
-Bout = Bout(1:Ny/FOVERSAMPL, 1:Nx/FOVERSAMPL);
-
-if EXPAND
-    Bout = Bout(size(Bino, 1)+1:2*size(Bino, 1), size(Bino, 2)+1:2*size(Bino, 2));
 end
 end
