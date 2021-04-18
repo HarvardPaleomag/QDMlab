384586ae225c6eaf1b0e839f9a92a59eea0a5e78
Mike Volk <michaelvolk1979@gmail.com>
2021-04-18 17:50:07 -0400

    started rewrite dipole_fit function

---
+++ fitting/dipole_fit.m
@@ -0,0 +1,401 @@
+function dipole_fit(kwargs)
+% MORDER is highest order in the multipole expansion: 2 is quadrupole; 8 is
+% octupole
+
+arguments
+    kwargs.dataFile = 'none';
+    kwargs.morder = 1;
+    kwargs.xy = [0 0];
+    kwargs.CROPFACT;
+    kwargs.downSample = 1;  %WHY?
+    kwargs.NRUNS;
+    kwargs.QUAD;
+    kwargs.outputtrue; 
+    kwargs.checkPlot;
+    
+    kwargs.incl = -0;
+    kwargs.dec = 0;
+    incl=40;
+    dec=300;
+
+    kwargs.m0 = 1e-12;
+    kwargs.hguess = 2.5e-5;
+    kwargs.minheight = 0;
+    kwargs.maxheight = 100e-6;
+    kwargs.boxwidth = 100e-6;
+
+    kwargs.METHOD = 1; %0 = least squares, 1=Nelder-Mead
+    kwargs.NOISE = 0; %0 = no noise
+    kwargs.SNR = 0; %signal-to-noise ratio in dB
+    kwargs.AUTO = 0; %automatically find dipole position from Bt map
+    kwargs.TERMS = [3, 8, 15];
+    kwargs.MINTOL = 1;
+    kwargs.stats = 0;
+    kwargs.nStats = 50; % n iterations for statistics 
+    kwargs.DISPLAY = 0;
+    
+end
+
+theta0 = 90 - kwargs.incl;
+phi0 = kwargs.dec + 90; %SM y axis is the one pointing northy
+
+change random number generator
+rng('shuffle')
+
+set(0, 'DefaultFigureColormap', jet)
+
+outFileName = 'DipoleInversions.txt';
+
+counter = kwargs.nStats;
+
+dataFile = automatic_input_ui__(kwargs.dataFile, 'type', 'file', 'single', true);
+
+expData = load(dataFile);
+[filePath, name, ext] = fileparts(dataFile);
+
+if is_B111(expData)
+    dtype = 'B111ferro';
+else
+    dype = 'Bz';
+end
+
+while counter
+
+    if kwargs.stats
+        fprintf('<>   Iteration %02i/%02i ...\n', kwargs.nStats - counter + 1, kwargs.nStats);
+    end
+    
+    step = 0;
+
+    if kwargs.downSample
+        Bdata = downsample(downsample(expData.(dtype), kwargs.downSample)', kwargs.downSample)';
+        step = step * kwargs.downSample;
+    end
+
+    scan = Bdata; % Bz is assumed in T
+
+    x = ((1:size(scan, 2)) - 1) * step;
+    y = ((1:size(scan, 1)) - 1) * step;
+    [X, Y] = meshgrid(x, y);
+    
+    if ~kwargs.stats
+        statsFigure = figure();
+    end
+    
+    if kwargs.NOISE
+        noise=randn(size(scan))*0.5*std(std(scan));
+        noise = sqrt(10^(-SNR / 10)*var(scan(:))) * randn(size(scan));
+        scan = scan + noise;
+    end
+
+    if kwargs.DISPLAY
+        NRUNS = 1;
+    end
+
+    j = round(XY(1)/kwargs.downSample);
+    i = round(XY(2)/kwargs.downSample);
+
+    x0 = j * step;
+    y0 = i * step;
+    cropj = round(1+j+[-CROPFACT, CROPFACT]);
+    cropi = round(1+i+[-CROPFACT, CROPFACT]);
+
+    %adjust if the crop area falls outside the image
+    scansize = size(scan);
+    
+    for p = 1:2
+        if cropj(p) < 1
+            cropj(p) = 1;
+        end
+        if cropi(p) < 1
+            cropi(p) = 1;
+        end
+        if cropj(p) > scansize(2)
+            cropj(p) = scansize(2);
+        end
+        if cropi(p) > scansize(1)
+            cropi(p) = scansize(1);
+        end
+    end
+
+    scanc = scan(cropi(1):cropi(2), cropj(1):cropj(2));
+    Xc = X(cropi(1):cropi(2), cropj(1):cropj(2));
+    Yc = Y(cropi(1):cropi(2), cropj(1):cropj(2));
+    xc = x(cropj(1):cropj(2));
+    yc = y(cropi(1):cropi(2));
+
+    imagesc(xc, yc, abs(Bz(cropi(1):cropi(2), cropj(1):cropj(2))));
+    axis xy, axis equal, axis tight
+    caxis([0, 1]*max(abs(caxis)));
+    colormap(hot)
+    colorbar
+    title(sprintf('Cropped map'));
+    drawnow
+
+    P00(1) = x0;
+    P00(2) = y0;
+    P00(3) = hguess;
+
+    drawnow
+    P = zeros(length(P00)+TERMS(MORDER), NRUNS);
+    fval = zeros(1, NRUNS);
+    fval2 = zeros(1, NRUNS);
+
+    for k = 1:NRUNS
+        if NRUNS == 1
+            P0 = P00; %+0.3*(rand(size(P00))-0.5).*P00;
+        else
+            %P0=P00+0.1*(rand(size(P00))-0.5).*[P00(1) 20 40 P00(4:6)];
+            P0 = P00 + 0.1 * (rand(size(P00)) - 0.5) .* P00;
+        end
+        
+        options = optimset('TolX', 10^(floor(log10(m0)) - 5), ...
+                           'TolFun', 10^(floor(log10(max(abs(Bz(:))))) - 8), ...
+                           'MaxFunEvals', 6000, 'MaxIter', 2000, ...
+                           'Display', 'none');
+
+        if METHOD
+            [P(1:3, k), fval2(k), exitflag, output] = fmincon(@(Pp) SourceFitMultiP8(Pp, Xc, Yc, scanc, DISPLAY, METHOD, QUAD, MORDER), P0, [], [], [], [], [x0 - boxwidth, y0 - boxwidth, 2e-5], [x0 + boxwidth, y0 + boxwidth, 3e-5], [], options);
+        else
+            [P(1:3, k), fval2(k), resd, exitflag, output] = lsqnonlin(@(Pp) SourceFitMultiP8(Pp, Xc, Yc, scanc, DISPLAY, METHOD, QUAD, MORDER), P0, [], [], options);
+        end
+        
+        [resid, BzModel, M] = SourceFitMultiP8(P(1:3, k), Xc, Yc, scanc, DISPLAY, METHOD, QUAD, MORDER);
+
+        Mx = M(1);
+        My = M(2);
+        Mz = M(3);
+        m = sqrt(Mx^2+My^2+Mz^2);
+        
+        theta = acosd(Mz/m);
+        phi = atan2d(My, Mx);
+        
+        P(4, k) = m;
+        %convert angles to inclination and declination
+        P(5, k) = 90 - theta;
+        P(6, k) = phi - 90;
+
+        %enforce range for parameters
+        change = 1;
+        while change
+            change = 0;
+            if P(5, k) > 90
+                P(5, k) = 180 - P(5, k); % i' = 180-i
+                P(6, k) = P(6, k) + 180; % d' = d+180
+                change = 1;
+            end
+            if P(5, k) < -90
+                P(5, k) = -180 - P(5, k); % i' = -180-i
+                P(6, k) = P(6, k) + 180;  % d' = d+180
+                change = 1;
+            end
+            if P(6, k) < 0
+                P(6, k) = P(6, k) + 360; % d' = d+360
+                change = 1;
+            end
+            if P(6, k) >= 360
+                P(6, k) = P(6, k) - 360; % d' = d-360
+                change = 1;
+            end
+        end
+        
+        if MORDER > 1
+            %Quadrupole moment
+            P(7, k) = M(4);
+            P(8, k) = M(5);
+            P(9, k) = M(6);
+            P(10, k) = M(7);
+            P(11, k) = M(8);
+        end
+
+        if MORDER > 2
+            %Octupole moment
+            P(12, k) = M(9);
+            P(13, k) = M(10);
+            P(14, k) = M(11);
+            P(15, k) = M(12);
+            P(16, k) = M(13);
+            P(17, k) = M(14);
+            P(18, k) = M(15);
+        end
+        
+        if kwargs.DISPLAY
+            fprintf('Moment: %.4e\n', P(4))
+            fprintf('Inclination: %.1f\n', P(5))
+            fprintf('Declination: %.1f\n', P(6))
+            fprintf('Height: %.4e\n', P(3))
+        end
+
+        fprintf('..%0d..(%0d)  ', k, output.iterations);
+        
+        if ~mod(k, 10)
+            fprintf('\n');
+        end
+        
+        if size(P, 1) > 6
+            Paux = [P(1:4, k)', 90 - P(5, k), P(6, k) + 90, P(7:TERMS(MORDER) + 3, k)'];
+        else
+            Paux = [P(1:4, k)', 90 - P(5, k), P(6, k) + 90];
+        end
+        
+        [resid, Bzmodel] = SourceFitMultiP8(Paux, Xc, Yc, scanc, 0, METHOD, QUAD, MORDER);
+        fval(k) = sqrt(sum(sum((Bzmodel - scanc).^2))/numel(scanc));
+    end
+    
+    i0 = find(fval == min(fval));
+    fsort = sort(fval);
+
+    %i=find(fval<=min(fval)+MINTOL*(max(fval)-min(fval)));
+    i = find(fval <= fsort(MINTOL));
+    fprintf('\n--- Averaging %d points ---\n', numel(i))
+    if numel(i) > 0.1 * numel(fval)
+        disp('Too many points are being averaged. Consider adjusting MINTOL parameter.')
+    end
+
+
+    Popt = zeros(size(Paux));
+    Popt(4) = sum(P(4, i).*fval(i)) / sum(fval(i));
+    mopt = Popt(4); %*1000
+    fprintf('(min = %1.3d)\n', P(4, i0));
+
+    Popt(5) = sum(P(5, i).*fval(i)) / sum(fval(i));
+    iopt = Popt(5);
+    fprintf('(min = %1.3f)\n', P(5, i0));
+
+    Popt(6) = sum(P(6, i).*fval(i)) / sum(fval(i));
+    dopt = Popt(6); %x and y axis are reversed in SM - N is Y instead of x
+    mod(360-dopt, 360)
+    fprintf('(min = %1.3f)\n', P(6, i0));
+
+    Popt(1) = sum(P(1, i).*fval(i)) / sum(fval(i));
+    Popt(2) = sum(P(2, i).*fval(i)) / sum(fval(i));
+    Popt(3) = sum(P(3, i).*fval(i)) / sum(fval(i));
+    hopt = Popt(3);
+    fprintf('(min = %1.3d)\n', P(3, i0));
+
+    for kk = 7:TERMS(MORDER) + 3
+        Popt(kk) = sum(P(kk, i).*fval(i)) / sum(fval(i));
+    end
+
+    xopt = Popt(1);
+    fprintf('(min = %1.3d)\n', P(1, i0));
+
+    yopt = Popt(2);
+    fprintf('(min = %1.3d)\n', P(2, i0));
+
+    Popt2 = [Popt(1:4), 90 - Popt(5), Popt(6) + 90, Popt(7:TERMS(MORDER) + 3)];
+    [resid, Bzmodel] = SourceFitMultiP8(Popt2, Xc, Yc, scanc, 0, METHOD, QUAD, MORDER);
+    residex = Bzmodel - scanc;
+    
+    if ~kwargs.stats
+        figure
+        subplot(2, 2, 1);
+        imagesc(xc, yc, scanc);
+        axis xy, axis equal, axis tight;
+        caxis([-1, 1]*max(abs(caxis)));
+        colorbar
+        title('Original Scan');
+        subplot(2, 2, 2);
+        imagesc(xc, yc, Bzmodel);
+        axis xy, axis equal, axis tight;
+        caxis([-1, 1]*max(abs(caxis)));
+        colorbar
+        title('Model Scan');
+        subplot(2, 2, 4);
+        imagesc(xc, yc, residex);
+        axis xy, axis equal, axis tight;
+        caxis([-1, 1]*max(abs(caxis)));
+        colorbar
+        title('Residuals');
+    end
+    
+    saveas(gcf, [filePath, '/Fit_', name, '_M', num2str(MORDER), '_x', num2str(round(XY(1))), 'y', num2str(round(XY(2))), '.png'])
+
+    %resids=sqrt(sum(sum(residex.^2))/numel(residex))
+    resids = sqrt(sum(sum(residex.^2))/sum(sum(scanc.^2)))
+
+    nameext = [name, ext];
+    if kwargs.stats
+        outputtrue = 1;
+    end
+
+    if outputtrue
+        fid = fopen([filePath, '/', outFileName], 'r');
+        header = (fid == -1);
+        if ~header
+            fclose(fid);
+        end
+        fid = fopen([filePath, '/', outFileName], 'a+t');
+        if header
+            fprintf(fid, 'File Name\tMoment\tInclination\tDeclination\tHeight\tResiduals\r\n');
+        end
+        %Note a 180 rotation about y axis is imposed here
+        if hopt < 0
+            fprintf(fid, '%s\t%1.5d\t%1.5d\t%1.5d\t%1.5d\t%1.5d\r\n', nameext, mopt, -iopt, mod(180 - dopt, 360), -hopt, resids);
+        else
+            fprintf(fid, '%s\t%1.5d\t%1.5d\t%1.5d\t%1.5d\t%1.5d\r\n', nameext, mopt, -iopt, mod(360 - dopt, 360), hopt, resids);
+        end
+        fclose(fid);
+    end
+
+    counter = counter - 1;
+
+end
+end
+
+function checkPlotFigure(P, fval)
+
+figure
+plot(P(1, :), fval, '.')
+title('Moment');
+hold on
+plot(P(1, i), fval(i), 'r.')
+plot(P(1, i0), fval(i0), 'c.')
+plot([mopt, mopt], ylim, 'm--');
+hold off
+
+figure
+plot(P(2, :), fval, '.')
+title('Inclination');
+hold on
+plot(P(2, i), fval(i), 'r.')
+plot(P(2, i0), fval(i0), 'c.')
+plot([iopt, iopt], ylim, 'm--');
+hold off
+
+figure
+plot(P(3, :), fval, '.')
+title('Declination');
+hold on
+plot(P(3, i), fval(i), 'r.')
+plot(P(3, i0), fval(i0), 'c.')
+plot([dopt, dopt], ylim, 'm--');
+hold off
+
+figure
+plot(P(6, :), fval, '.')
+title('Height');
+hold on
+plot(P(6, i), fval(i), 'r.')
+plot(P(6, i0), fval(i0), 'c.')
+plot([hopt, hopt], ylim, 'm--');
+hold off
+
+figure
+plot(P(4, :), fval, '.')
+title('X displacement');
+hold on
+plot(P(4, i), fval(i), 'r.')
+plot(P(4, i0), fval(i0), 'c.')
+plot([xopt, xopt], ylim, 'm--');
+hold off
+
+figure
+plot(P(5, :), fval, '.')
+title('Y displacement');
+hold on
+plot(P(5, i), fval(i), 'r.')
+plot(P(5, i0), fval(i0), 'c.')
+plot([yopt, yopt], ylim, 'm--');
+hold off
+end
