4e75f77e2872b3f28ef9a6a4ce283a5fc5b36797
Mike Volk <michaelvolk1979@gmail.com>
2021-04-25 18:12:41 -0400

    adds get_laser function

---
+++ utilities/get_laser.m
@@ -0,0 +1,26 @@
+function laserData = get_laser(path, kwargs)
+
+    arguments
+        path
+        kwargs.data = 'none'
+    end
+    
+    if ~strcmp(kwargs.data, 'none')
+        if exists_struct(kwargs.data, 'laser')
+            laserData = kwargs.data.laser;
+            return
+        end
+    end
+    
+    laserFiles = dir(fullfile(path,'laser.*'));
+    [~,~,ext] = fileparts(laserFiles(1).name);
+    
+    if strcmp(ext, '.jpg')
+        laserData = imread(fullfile(path, laserFiles(1).name))
+        return
+    end
+    
+    if strcmp(ext, '.csv')
+        laserData = load(fullfile(path, laserFiles(1).name));
+    end
+end
