51d6dfed8d7e868017e5fbb66bc37a454866c30c
Mike Volk <michaelvolk1979@gmail.com>
2021-04-27 17:08:38 -0400

    fixes bug with wrong name for saving

--- map_editing/crop_map.m
+++ map_editing/crop_map.m
@@ -20,27 +20,27 @@ dataFile = automatic_input_ui__(kwargs.dataFile, 'type', 'file', 'title', 'Pick 
 expData = load(dataFile{:});
 [~, dataName, ~] = is_B111(expData);
 
-[lin, col] = pick_box2('expData', expData, 'title', 'Select area to crop');
+[row, col] = pick_box2('expData', expData, 'title', 'Select area to crop');
 
 %enforce that the cropped array has even dimensions
 xrange=col(2)-col(1);
-yrange=lin(2)-lin(1);
+yrange=row(2)-row(1);
 if mod(xrange,2)
     col(2,1)=col(2,1);
 else
     col(2,1)=col(2,1)-1;
 end
 if mod(yrange,2)
-    lin(2,1)=lin(2,1);
+    row(2,1)=row(2,1);
 else
-    lin(2,1)=lin(2,1)-1;
+    row(2,1)=row(2,1)-1;
 end
 
 
 bData = expData.(dataName);
 %cropping a B111 map set
-corners=[lin(1),lin(2);col(1),col(2)];
-bDataCropped=bData(lin(1):lin(2),col(1):col(2));
+corners=[row(1),row(2);col(1),col(2)];
+bDataCropped=bData(row(1):row(2),col(1):col(2));
 
 expData.(dataName) = bDataCropped;
 expData.([dataName '_original']) = bData;
@@ -58,8 +58,7 @@ if kwargs.save
     
     iFileNew = strrep(dataFile{1,1}, '.mat','_Cropped.mat');
     fprintf('<>     SAVING: cropped data to file << %s >>\n', iFileNew);
-    
-    saveas(fig,fullpath(filePath, fileName, '_Hole.png'))
+    saveas(fig,fullfile(filePath, sprintf('%s_Hole.png', fileName)))
     save(iFileNew,'-struct','expData');
 end
 
