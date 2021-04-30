d2ffb34e60dc75948e2491b73ddeb92bfeb20217
Michael Volk <michaelvolk1979@gmail.com>
2021-04-30 14:20:12 -0400

    binImage now uses imresize -> 100x faster

--- utilities/prepare_raw_data.m
+++ utilities/prepare_raw_data.m
@@ -51,7 +51,7 @@ data = permute(data,[3 2 1]); % permute the axis to rows x cols x freq
 
 % binning
 fprintf('<>   %i: binning data >> binSize = %i\n', nRes, binSize);
-binData = imresize(data, 1/binSize, 'method', 'box');
+binData = BinImage(data,binSize);
 
 % Correct for severely non-unity baseline by dividing pixelwise by
 % average of all frequency points
