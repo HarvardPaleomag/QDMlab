352630f79ec82fa6bfade6a34bd4f7950a1fe48e
Michael Volk <michaelvolk1979@gmail.com>
2021-05-04 17:44:23 -0400

    fixes issue where led is not cropped correctly

--- Bz_and_UpCont/QDMdataprocessing.m
+++ Bz_and_UpCont/QDMdataprocessing.m
@@ -156,10 +156,10 @@ corners=[lin,col];
 B=B(max([1 lin(1)]):min([lin(2) size(B,1)]) , max([1 col(1)]):min([col(2) size(B,2)]) );
 
 %crop the LED image in the same way
-croppoint1=min(1, round(corners(1,1)*LEDcropfactor));
-croppoint2=max(size(ledImg,1), round(corners(2,1)*LEDcropfactor));
-croppoint3=min(1, round(corners(1,2)*LEDcropfactor));
-croppoint4=max(size(ledImg,2), round(corners(2,2)*LEDcropfactor));
+croppoint1=round(corners(1,1)*LEDcropfactor);
+croppoint2=round(corners(2,1)*LEDcropfactor);
+croppoint3=round(corners(1,2)*LEDcropfactor);
+croppoint4=round(corners(2,2)*LEDcropfactor);
 
 newLED=ledImg(croppoint1:croppoint2,croppoint3:croppoint4);
 
