214c3800550e2632e2e047d8344f6ac0a79195a6
Mike Volk <michaelvolk1979@gmail.com>
2021-05-04 09:27:14 -0400

    errors displayed in red

--- utilities/logMsg.m
+++ utilities/logMsg.m
@@ -8,7 +8,7 @@ arguments
     kwargs.returnOnly = false
 end
 
-levels = containers.Map({'DEBUG','INFO', 'WARN', 'ERROR', 'NONE'}, {0, 1 ,2 ,3, 4});
+levels = containers.Map({'ALL', 'DEBUG','INFO', 'WARN', 'ERROR', 'NONE'}, {-1, 0, 1 ,2 ,3, 4});
 
 try 
     logLevel = evalin('base','logLevel');
@@ -41,9 +41,11 @@ indent = repmat('  ',1,indent);
 msg = sprintf('<> %6s %s: %s:%s %s', datetime('now', 'Format', 'HH:mm:ss:SS'), ...
               pad(upper(level), 6,'left'),...
               ['QDMlab.' callerName], indent, message);
+
+% while msg           
           
 if ~kwargs.returnOnly
-    if strcmpi(level, 'INPUT')
+    if strcmpi(level, 'INPUT') | strcmpi(level, 'ERROR')
         if lineEnd
             fprintf(2, '%s\n',msg);
         else
@@ -56,8 +58,39 @@ if ~kwargs.returnOnly
             fprintf('%s',msg);
         end
     end
+    
 elseif kwargs.returnOnly && lineEnd
     msg = sprintf('%s\n',msg);
 end
 
 end
+
+
+function newMsg = split_msg(textWidth, msg, message)
+% function clips the text to a fixed length
+% it does not work properly and can clip text if it becomes too large
+
+    nChar = size(msg,2);
+    nSplits = floor(nChar/textWidth);
+    spaces = find(msg==' ');
+    newMsg = '';
+    
+    if nSplits
+        logChars = numel(strrep(msg,message, ''));
+        indentation = repmat(' ',1,logChars);
+        splits = [];
+        for i = 1:nSplits
+            n = i*textWidth;
+            [~, spaceIdx] = min(abs(spaces - i*textWidth));
+            [~, nextSpaceIdx] = min(abs(spaces - (i+1)*textWidth));
+
+            lineString = msg(spaces(spaceIdx):spaces(nextSpaceIdx));
+            if i == 1
+                newMsg = [msg(1:spaces(spaceIdx)-1) '\n'  repmat(' ',1,logChars) lineString];
+            else
+                newMsg = [newmsg '\n' repmat(' ',1,logChars) lineString];
+            end
+        end
+    end
+end
+    
