013b35f479bb11edc61eb89afc6d24279ecf1a66
Mike Volk <michaelvolk1979@gmail.com>
2021-05-30 14:39:44 -0400

    more tests

--- testing/testing_general.m
+++ testing/testing_general.m
@@ -92,4 +92,47 @@ ax2 = subplot(1,2,2);
 QDM_figure(dipSub.Bz, 'ax', ax2);
 linkaxes([ax1 ax2]);
 
+%% dipole fit
+close all; clc;
+dfit = dipole_fit('filePath', milnrmBz, 'fitOrder', 1, 'cropFactor', 10, 'save',0, 'xy', [334,254]);
+pause(2)
+close all
+dfitc = dipole_fit('filePath', milnrmBz, 'fitOrder', 1, 'cropFactor', 10, 'save',0, 'constrained', true, 'xy', [334,254]);
+pause(2)
+close all
+dfitcMinMax = dipole_fit('filePath', milnrmBz, 'fitOrder', 1, 'cropFactor', 10, 'save',0, 'constrained', true, 'maxheight', 10e-6, 'minheight', 3e-6, 'xy', [334,254]);
+pause(2)
+close all
+f = figure('units','normalized','outerposition',[0.2 0.4 0.4 0.5],'NumberTitle', 'off', 'Name', 'Dipole fitting comparison');
+ax1 = subplot(2,2,1);
+QDM_figure(dfit.data, 'ax', ax1);
+axis xy; axis equal; axis tight;
+
+ax2 = subplot(2,2,2);
+QDM_figure(dfit.model, 'ax', ax2, 'title', 'unconstrained');
+ax3 = subplot(2,2,3);
+QDM_figure(dfitc.model, 'ax', ax3, 'title', 'constrained');
+ax4 = subplot(2,2,4);
+QDM_figure(dfitcMinMax.model, 'ax', ax4, 'title', 'constrained h = [3e-6,10e-6]');
+linkaxes([ax1 ax2 ax3 ax4]);
+
 %%
+clc
+% dipfitSer = dipole_fit_series(strrep(milnrmBz, '/Bz_uc0.mat', ''));
+dipfitSerConst = dipole_fit_series(strrep(milnrmBz, '/Bz_uc0.mat', ''), 'constrained', true, 'minheight', 5e-6, 'maxheight', 5e-6);
+%%
+f = figure('units','normalized','outerposition',[0.2 0.4 0.3 0.5],'NumberTitle', 'off', 'Name', 'dipole_fit_series constrains');
+ax1 = subplot(2,2,1);
+QDM_figure(dipfitSer.data{1}, 'ax', ax1);
+axis xy; axis equal; axis tight;
+
+ax2 = subplot(2,2,2);
+QDM_figure(dipfitSer.model{1}, 'ax', ax2);
+ax3 = subplot(2,2,3);
+QDM_figure(dipfitSerConst.data{1}, 'ax', ax3);
+axis xy; axis equal; axis tight;
+ax4 = subplot(2,2,4);
+QDM_figure(dipfitSerConst.model{1}, 'ax', ax4);
+
+linkaxes([ax1 ax2])
+linkaxes([ax3 ax4]);
