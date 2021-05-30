e7f33bd8b1437a4266dc2470e7d3f46e9b847c37
Mike Volk <michaelvolk1979@gmail.com>
2021-05-30 13:04:05 -0400

    some bugfixes to subtract_constant

--- map_editing/subtract_constant.m
+++ map_editing/subtract_constant.m
@@ -20,25 +20,32 @@ bData = expData.(dataName);
 const = bData(lin,col);
 bData = bData - const;
 
-fprintf('<>   INFO: Constant %s value of %.2f subtracted from map', dataName, const);
+msg = sprintf('Constant %s value of %.2e subtracted from map', dataName, const);
+logMsg('info',msg,1,0);
+
+[fPath,fileName,~]=fileparts(filePath{1,1});
 
 if kwargs.editFigure
     fig = figure('Units', 'normalized', ...
                  'Position',[0.2 0.2 0.5 0.5], 'Name', 'edited map');
-    QDM_figure(bData, 'title', 'cropped map', 'fig', fig)
+    QDM_figure(bData, 'title', 'edited map', 'fig', fig);
+    
+    if kwargs.save
+        imagePath = fullfile(fPath, sprintf('%s_%sSub.png', fileName, dataName));
+        msg = sprintf('saving image << %s >>', imagePath');
+        logMsg('info',msg,1,0);
+        saveas(fig, imagePath)
+    end
 end
 
 if kwargs.save
     % save edited map to struct 
     expData.([dataName '_original']) = expData.(dataName);
     expData.(dataName) = bData;
-
-    [fPath,fileName,~]=fileparts(filePath{1,1});
-
-    iFileNew = strrep(filePath{1,1}, '.mat', sprintf('_%sSub.mat', dataName));
-    fprintf('<>     SAVING: cropped data to file << %s >>\n', iFileNew);
     
-    saveas(fig,fullfile(fPath, sprintf('%s_%sSub.png', fileName, dataName)))
+    iFileNew = strrep(filePath{1,1}, '.mat', sprintf('_%sSub.mat', dataName));
+    msg = sprintf('saving edited data to file << %s >>', iFileNew');
+    logMsg('info',msg,1,0);
     save(iFileNew,'-struct','expData');
 end
 
