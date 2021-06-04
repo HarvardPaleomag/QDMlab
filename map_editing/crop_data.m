854d8a92acc97ef01848227729ea0cf00a60b98c
Mike Volk <michaelvolk1979@gmail.com>
2021-06-04 19:54:35 -0400

    docstring changes

--- map_editing/crop_data.m
+++ map_editing/crop_data.m
@@ -1,17 +1,16 @@
 function croppedData = crop_data(data, mask)
 %[croppedData] = crop_data(data, mask)
-%{
-Helper function to crop data according to a specified mask
+% Helper function to crop data according to a specified mask
+% 
+% Parameters
+% ----------
+%     positional
+%     ----------
+%     data: array
+%         the data
+%     mask: logical-array
+%         the mask consisting of 0,1
 
-Parameters
-----------
-    positional
-    ----------
-    data: array
-        the data
-    mask: logical-array
-        the mask consisting of 0,1
-%}
     [x, y, w, h] = get_mask_extent(mask);
 
     % cut around data
