5fbf20704f0532537c4332003dab87fac4c59f98
Michael Volk <michaelvolk1979@gmail.com>
2021-06-08 16:27:40 -0400

    small changes to test

--- testing/testing_code.m
+++ testing/testing_code.m
@@ -2,15 +2,26 @@
 % expData = load('/Volumes/backups/antares/mike/MIL/MIL3/FOV1/NRM/14G/4x4Binned_GF2/final_fits_(4x4).mat');
 % expData = load('Z:\antares\mike\MIL\MIL3\FOV1\NRM\4x4Binned\final_fits_(4x4).mat');
 % expData = load('/Volumes/backups/antares/tiled_blanks/mariner/FOV_2_2/final_fits_(4x4).mat');
-expData = load('/Users/mike/Dropbox/science/_projects/QDMlab_paper/data/QDM_data/blanks/2021_05_11/final_fits_(4x4).mat');
-% expData = load('/Users/mike/Dropbox/science/_projects/QDMlab_paper/data/QDM_data/speleothems/10-20-10/final_fits_(4x4).mat');
+% expData = load('/Users/mike/Dropbox/science/_projects/QDMlab_paper/data/QDM_data/blanks/2021_05_11/final_fits_(4x4).mat');
+expData = load('/Users/mike/Dropbox/science/_projects/QDMlab_paper/data/QDM_data/speleothems/10-20-10/final_fits_(4x4).mat');
 b = expData.B111ferro;
 %%  
-% close all 
+close all 
 clc
-[d1 d2 d3 d4] = B111(expData, 'fieldsPlot', 1, 'centerShiftPlot', 1, 'resonanceFieldPlot', 1);
+data = B111(expData, 'fieldsPlot', 0, 'centerShiftPlot', 0, 'resonanceFieldPlot', 0, 'centerShiftDistribution',1);
+d1_ = filter_hot_pixels(data.ferro, 'threshold',false,'cutoff', 15);
+d3_ = QuadBGsub(data.centerShift);
 % [d1 d2 d3 d4] = B111(expData, 'centerShiftPlot', 1);
 % [d1 d2 d3 d4] = B111(expData, 'resonanceFieldPlot', 1);
+%%
+close all
+ferro = QuadBGsub(data.ferro);
+ferro = filter_hot_pixels(ferro,'cutoff',2, 'win', nan);
+CS = QuadBGsub(data.centerShift);
+CS = filter_hot_pixels(CS,'cutoff',2, 'win', nan);
+d = [reshape(ferro,[],1), reshape(CS,[],1)];
+
+d = rmmissing(d);
 %%
 close all
 yline(0)
@@ -22,18 +33,80 @@ ylabel('B111ferro')
 xlabel('centerShift')
 %%
 close all
-ax1 = subplot(1,2,1)
-ax2 = subplot(1,2,2)
-for i = linspace(0,0.01,500)
-    imagesc(ax1,filter_hot_pixels( d1-i*d3, 'threshold', false, 'cutoff', 10))
-    title(ax1,i)
+f = figure('units','normalized','outerposition',[0.2 0.4 0.6 0.8],'NumberTitle', 'off', 'Name', 'title');
+ax1 = subplot(2,2,1);
+ax2 = subplot(2,2,2);
+ax3 = subplot(2,2,3);
+ax4 = subplot(2,2,4);
+GFs = linspace(0,0.01,500);
+RMS = rand(size(GFs));
+
+for i = 1: size(GFs,2)
+    GF = GFs(i);
+    d = d1_ - GF*d3_;
     
+    RMS(i) = rms(d,'all');
     
-    hold(ax2, 'on')
-    plot(ax2, i, rms(d1-i*d3, 'all'), '.k')
-    drawnow
-    pause(0.01)
+    if i~= 1 && RMS(i) > 1.01*RMS(i-1)
+        break
+    end
 end
+[m,idx] = min(RMS);
+QDM_figure(d1_, 'ax',ax1, 'title', 'data');
+QDM_figure(d1_ - m*d3_, 'ax',ax2, 'title', 'corrected');
+QDM_figure(m*d3_, 'ax',ax3, 'title', '\Delta');
+plot(ax4, GFs(1:i), RMS(1:i));
+set(ax4,'Position', [ax4.Position(1:2), ax1.Position(3:4)]);
+xlabel('GF');
+ylabel('RMS');
+hold(ax4, 'on');
+plot(GFs(idx), RMS(idx), 'Xr','LineWidth',2);
+title(ax4, sprintf('optimal: %.4f, %s RMS: %.2f %%', GFs(idx), '\Delta', 100*(RMS(idx)/RMS(1)-1)))
+linkaxes([ax1 ax2 ax3])
+
+%%
+close all
+f = figure('units','normalized','outerposition',[0.2 0.4 0.6 0.8],'NumberTitle', 'off', 'Name', 'title');
+ax1 = subplot(2,2,1)
+QDM_figure(d1_, 'ax',ax1, 'title', 'data', 'clim',[-6e-3,6e-3]);
+ax2 = subplot(2,2,2);
+[f, ax2, im2] = QDM_figure(d1_ - 0*d3_, 'ax',ax2, 'title', 'corrected', 'clim',[-6e-3,6e-3]);    
+ax3 = subplot(2,2,3);
+[f, ax3, im3] = QDM_figure(0*d3_, 'ax',ax3, 'title', '\Delta', 'clim',[-1e-3,1e-3]);
+ax4 = subplot(2,2,4);
+
+GFs = linspace(0,0.03,10);
+RMS = rand(size(GFs));
+
+for i = 1: size(GFs,2)
+
+    GF = GFs(i);
+    d = d1_ - GF*d3_;
+    
+    set(im2, 'CData', d1_ - GF*d3_);
+    set(im3, 'CData', GF*d3_);
+    
+    RMS(i) = rms(d,'all');
+    plot(ax4, GFs(1:i), RMS(1:i),'.k','LineWidth',1.5);
+    set(ax4,'Position', [ax4.Position(1:2), ax1.Position(3:4)]);
+    xlabel(ax4, 'GF');
+    ylabel(ax4, 'RMS');
+    xlim(ax4, [0,max(GFs)])
+    drawnow
+end
+p = polyfit(GFs,RMS,2);
+r = roots(polyder(p));
+[m,idx] = min(RMS);
+set(im2, 'CData', d1_ - GFs(idx)*d3_);
+set(im3, 'CData', GFs(idx)*d3_);
+xline(ax4, GFs(idx));
+hold(ax4, 'on');
+plot(linspace(0,max(GFs),100), polyval(p, linspace(0,max(GFs),100)))
+plot(r, rms(d1_ - r*d3_,'all'), 'Xr','LineWidth',2);
+plot(GFs(idx), RMS(idx), 'or','LineWidth',2);
+title(ax4, sprintf('optimal: %.4f, %s RMS: %.2f %%', GFs(idx), '\Delta', 100*(RMS(idx)/RMS(1)-1)))
+linkaxes([ax1 ax2 ax3]);
+
 %%
 close all
 imagesc(filter_hot_pixels( d1-0*d3, 'threshold', false, 'cutoff', 5))
@@ -52,91 +125,3 @@ ax1 = subplot(1,1,1);
 hold on
 
 QDM_figure(b, 'alpha', 1e-13, 'ax', ax1, 'xc', 1:960, 'yc', 1:600, 'title', 'QDM')
-
-% QDM_figure(b, 'alpha', 1, 'ax', ax)
-%%
-function [b111Ferro, b111Para, centerShift, dDelta] = B111(expData, plots)
-    arguments
-        expData
-        plots.resonanceFieldPlot {mustBeBoolean(plots.resonanceFieldPlot)} = false
-        plots.centerShiftPlot {mustBeBoolean(plots.centerShiftPlot)} = false
-        plots.fieldsPlot {mustBeBoolean(plots.fieldsPlot)} = false
-    end
-    
-    gamma = 0.0028;
-    
-    resPos1 = expData.leftPos.resonance;
-    resPos2 = expData.rightPos.resonance;
-    
-    resNeg1 = expData.leftNeg.resonance;
-    resNeg2 = expData.rightNeg.resonance;
-    
-    medianResPos = median((resPos2+resPos1)/2, 'all', 'omitnan');
-    medianResNeg = median((resNeg2+resNeg1)/2, 'all', 'omitnan');
-    med = mean([medianResPos medianResNeg]);
-    
-    %% shift away from the mean
-    % negative fields
-    dNeg1 = (med-resNeg1)/gamma;
-    dNeg2 = (resNeg2-med)/gamma;
-    % positive fields
-    dPos1 = (med-resPos1)/gamma;
-    dPos2 = (resPos2-med)/gamma;
-    
-    %% mean shift (i.e. mean(field) with pos/neg field applied
-    meanPos =  (dPos1+dPos2)/2;
-    meanNeg =  (dNeg1+dNeg2)/2;
-    
-    %% ferro / para component calculation
-    b111Ferro = (meanPos - meanNeg)/2; 
-    b111Para  = (meanPos + meanNeg)/2;
-    
-    %% center shift in pos/neg field direction
-    deltaPos = (dPos2-dPos1)/2;
-    deltaNeg = (dNeg2-dNeg1)/2;
-    
-    %% average center shift
-    centerShift = (deltaPos + deltaNeg)/2;
-    
-    %% differences between pos/ neg field
-    dDelta = (deltaNeg - deltaPos)/2;
-    
-    %% PLOTTING
-    %% center shift
-    if isequal(plots.centerShiftPlot, true)
-        f = figure('units','normalized','outerposition',[0.15 0.4 0.7 0.4],'NumberTitle', 'off', 'Name', 'Center Shifts');
-        ax1 = subplot(1,2,1);
-        QDM_figure(deltaPos, 'ax', ax1, 'title', 'B^+');
-        ax2 = subplot(1,2,2);
-        QDM_figure(deltaNeg, 'ax', ax2, 'title', 'B^-');
-        linkaxes([ax1 ax2]);
-    end
-    
-    %% fields
-    if isequal(plots.fieldsPlot, true)
-        f = figure('units','normalized','outerposition',[0.1 0.1 0.8 0.8],'NumberTitle', 'off', 'Name', 'B111ferro, B111para');
-        ax1 = subplot(2,2,1);
-        QDM_figure(b111Ferro, 'st', 10, 'preThreshold', 10, 'ax', ax1, 'title', 'B_{111} ferro');
-        ax2 = subplot(2,2,2);
-        QDM_figure(b111Para-median(b111Para, 'all'), 'st', 10, 'preThreshold', 10, 'ax', ax2, 'title', 'B_{111} para', 'mustBe', false);
-        ax3 = subplot(2,2,3);
-        QDM_figure(centerShift, 'st', 20, 'preThreshold', 1, 'ax', ax3, 'title', 'mean(center shift)');
-        ax4 = subplot(2,2,4);
-        QDM_figure(dDelta, 'st', 2, 'preThreshold', 1, 'ax', ax4, 'title', 'CS^- - CS^+');
-        linkaxes([ax1 ax2 ax3 ax4]);
-    end
-    
-    %% resonance fields
-    if isequal(plots.resonanceFieldPlot, true)
-        f = figure('units','normalized','outerposition',[0.15 0.4 0.7 0.4],'NumberTitle', 'off', 'Name', 'Resonance Fields');
-        ax1 = subplot(2,2,1);
-        QDM_figure(dPos1-median(dPos1,'all'), 'ax', ax1, 'title', 'B^+_1');
-        ax2 = subplot(2,2,2);
-        QDM_figure(dPos2-median(dPos2,'all'), 'ax', ax2, 'title', 'B^+_2');
-        ax3 = subplot(2,2,3);
-        QDM_figure(dNeg1-median(dNeg1,'all'), 'ax', ax3, 'title', 'B^-_1');
-        ax4 = subplot(2,2,4);
-        QDM_figure(dNeg2-median(dNeg2,'all'), 'ax', ax4, 'title', 'B^-_2');
-        linkaxes([ax1 ax2 ax3 ax4]);
-    end
-end
