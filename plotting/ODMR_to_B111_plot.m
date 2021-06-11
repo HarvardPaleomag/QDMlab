fbace90deef58dbf48cca35ed13e921b6776b6b8
Michael Volk <michaelvolk1979@gmail.com>
2021-06-11 15:07:54 -0400

    moved save and added save option

--- plotting/ODMR_to_B111_plot.m
+++ plotting/ODMR_to_B111_plot.m
@@ -12,23 +12,18 @@ xc = 1:xc; yc = 1:yc;
 
 %f1=figure; imagesc( (negDiff) ); axis equal tight; caxis([-r2 -r1]); colorbar; colormap(gca, turbo(512)); title('Negative current B_{111} (gauss)'); set(gca,'YDir','normal');
 [f1, ~, ~] = QDM_figure(fits.negDiff, 'preThreshold', 14, 'title', 'Negative current B_{111} (gauss)', 'cbTitle', 'B_{111} (G)', 'xc', xc,'yc', yc, 'mustBe', 'neg');
-saveas(f1, fullfile(savePath, 'negCurrent.png'), 'png');
 
 %f2=figure; imagesc( (posDiff) ); axis equal tight; caxis([r1 r2]); colorbar; colormap(gca, turbo(512)); title('Positive current B_{111} (gauss)'); set(gca,'YDir','normal');
 [f2, ~, ~] = QDM_figure(fits.posDiff, 'preThreshold', 14, 'title', 'Positive current B_{111} (gauss)', 'cbTitle', 'B_{111} (G)', 'xc', xc,'yc', yc, 'mustBe', 'pos');
-saveas(f2, fullfile(savePath, 'posCurrent.png'), 'png');
 
 %f3=figure; imagesc( B111ferro ); axis equal tight; caxis(-.1 + [-rng rng]); colorbar; colormap(gca, turbo(512)); title('Positive + negative ferro B_{111} (gauss)'); set(gca,'YDir','normal');
 [f3, ~, ~] = QDM_figure(fits.B111ferro, 'preThreshold', 14, 'title', 'Positive + negative ferro B_{111} (gauss)', 'cbTitle', 'B_{111} (G)');
-saveas(f3, fullfile(savePath, 'ferromagImg.png'), 'png');
 
 %f4=figure; imagesc(ledImg); axis equal tight; colorbar; colormap(gca, gray(512)); caxis auto; title('LED image'); set(gca,'YDir','normal');
 [f4, ~, ~] = QDM_figure(fits.ledImg, 'preThreshold', 14, 'led', true, 'title', 'LED image', 'xc', xc,'yc', yc);
-saveas(f4, fullfile(savePath, 'ledImg.png'), 'png');
 
 %f5=figure; imagesc( B111para ); axis equal tight; caxis([r1 r2]); colorbar; colormap(gca, turbo(512)); title('Positive + negative para B_{111} (gauss)'); set(gca,'YDir','normal');
 [f5, ~, ~] = QDM_figure(fits.B111para, 'preThreshold', 14, 'title', 'Positive - negative ferro B_{111} (gauss)', 'cbTitle', 'B_{111} (G)', 'xc', xc,'yc', yc, 'mustBe', 'pos');
-saveas(f5, fullfile(savePath, 'paramagImg.png'), 'png');
 
 %% combined figure
 f6 = figure('Name', 'data', 'units', 'normalized', 'outerposition', [0, 0, 1, 1]);
@@ -44,7 +39,6 @@ QDM_figure(fits.ledImg, 'ax', s4, 'preThreshold', 14, 'led', true, 'title', 'LED
 sgtitle(', B111 points up and out of page');
 ax = [s1, s2, s3, s4];
 linkaxes(ax);
-saveas(f6, fullfile(savePath, 'allPlots.png'), 'png');
 
 map = [1, 1, 1; 1, 0, 0];
 f7 = figure;
@@ -53,5 +47,14 @@ axis equal tight;
 colormap(gca, map);
 title('pixel alerts');
 set(gca, 'YDir', 'normal');
-saveas(f7, fullfile(savePath, 'pixelAlerts.png'), 'png');
+
+if ~ isequal(savePath, false)
+    saveas(f1, fullfile(savePath, 'negCurrent.png'), 'png');
+    saveas(f2, fullfile(savePath, 'posCurrent.png'), 'png');
+    saveas(f3, fullfile(savePath, 'ferromagImg.png'), 'png');
+    saveas(f4, fullfile(savePath, 'ledImg.png'), 'png');
+    saveas(f5, fullfile(savePath, 'paramagImg.png'), 'png');
+    saveas(f6, fullfile(savePath, 'allPlots.png'), 'png');
+    saveas(f7, fullfile(savePath, 'pixelAlerts.png'), 'png');
+end
 end
