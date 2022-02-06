215fb235f6e5854479390141b41ddb0bf8fca065
Michael Volk <michaelvolk1979@gmail.com>
2022-02-06 14:07:45 -0500

    feat: now prints the frequencies for use in LabView

--- picking_tools/pick_fcrop.m
+++ picking_tools/pick_fcrop.m
@@ -15,6 +15,8 @@ function [idx] = pick_fcrop(meanData,freq)
     plot(freq(idx(1):idx(2)), meanData(idx(1):idx(2)), 'ok-');
     msg = sprintf('cropping data between index %i : %i >> reduced datapoints to %i', idx(1), idx(2), diff(idx)+1);
     logMsg('debug',msg,1,0);
+    msg = sprintf('New frequency Range: %i : %i', freq(idx(1)), freq(idx(2)));
+    logMsg('info',msg,1,0);
     pause(2)
     close gcf
 end
