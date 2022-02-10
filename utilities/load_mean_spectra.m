0a8119f98647b8b3b00d47f55006f256a7c4f055
Michael Volk <michaelvolk1979@gmail.com>
2022-02-10 16:09:16 -0500

    feat: better load

--- utilities/load_mean_spectra.m
+++ utilities/load_mean_spectra.m
@@ -1,4 +1,4 @@
-function [meanData freq] = load_mean_spectra(folder)
+function [meanData freq struct] = load_mean_spectra(folder)
 
 
 runFiles = dir(fullfile(folder, 'run_*.mat'));
@@ -7,10 +7,15 @@ for i = 1:size(runFiles)
     runFiles(i).data = load(fullfile(runFiles(i).folder, ... 
         runFiles(i).name), 'disp1', 'disp2', 'freqList');
 end
-
+struct = [];
 meanData = [];
 freq = [];
 for i = 1:size(runFiles)
+    struct(i).left = runFiles(i).data.disp1;
+    struct(i).right = runFiles(i).data.disp2;
+    struct(i).leftF = runFiles(i).data.freqList(1:size(runFiles(i).data.disp1,2));
+    struct(i).rightF = runFiles(i).data.freqList(size(runFiles(i).data.disp1,2)+1:end);
+    
     meanData = [meanData; [runFiles(i).data.disp1, runFiles(i).data.disp2]];
     freq = [freq; runFiles(i).data.freqList];
 end
