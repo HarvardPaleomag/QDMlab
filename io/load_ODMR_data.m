dcf9b069cf33819440e68213f105f30091f5fc9e
Mike Volk <m.w.r.volk@uu.nl>
2022-05-03 11:01:14 +0200

    feat: adds mean_without_nan for loading datafiles

--- io/load_ODMR_data.m
+++ io/load_ODMR_data.m
@@ -4,9 +4,14 @@ function runFiles = load_ODMR_data(folder)
 runFiles = dir(fullfile(folder, 'run_*.mat'));
 
 for i = 1:size(runFiles)
-    data = load(fullfile(runFiles(i).folder, ... 
-        runFiles(i).name), 'imgStack1', 'imgStack2', 'imgStack3', 'imgStack4', 'freqList');
-    
+    try
+        data = load(fullfile(runFiles(i).folder, ... 
+            runFiles(i).name), 'imgStack1', 'imgStack2', 'imgStack3', 'imgStack4', 'freqList');
+    catch
+        data = load(fullfile(runFiles(i).folder, ... 
+            runFiles(i).name), 'imgStack1', 'imgStack2', 'freqList');
+    end
+
     if isfield(data, 'imgStack3')
         runFiles(i).left = [data.imgStack1; data.imgStack2];
         runFiles(i).right = [data.imgStack3; data.imgStack4];
@@ -14,6 +19,10 @@ for i = 1:size(runFiles)
         runFiles(i).left = data.imgStack1;
         runFiles(i).right = data.imgStack2;
     end
+
+    runFiles(i).leftMean = mean_without_nan(runFiles(i).left);
+    runFiles(i).rightMean = mean_without_nan(runFiles(i).right);
+
     %% frequencies in GHz
     runFiles(i).leftF = data.freqList(1:size(runFiles(i).left))/1e9;
     runFiles(i).rightF = data.freqList(size(runFiles(i).left)+1:end)/1e9;
---
+++ utilities/mean_without_nan.m
@@ -0,0 +1,17 @@
+function meanD = mean_without_nan(data)
+% takes a QDM image stack and returns the mean for only those pixels, tat
+% do not contain NaN in the data
+
+% find pixels with nan
+nanPix = isnan(mean(data,1));
+
+nPix = numel(nonzeros(nanPix));
+
+if nPix >0
+    msg = sprintf('Calculating mean without << %i NaN >> pixels.', nPix);
+    logMsg('info',msg,1,0);
+end
+
+meanD = data(:,~nanPix);
+% calculate mean for remaining pixels
+meanD = mean(meanD, 2);
