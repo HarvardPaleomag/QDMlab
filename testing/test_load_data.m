0400bb061f56d0c203352242de56ee3d80c9f087
Mike Volk <m.w.r.volk@uu.nl>
2022-05-30 16:33:30 +0200

    feat: adds more tests

--- testing/test_load_data.m
+++ testing/test_load_data.m
@@ -3,6 +3,18 @@ clear
 logLevel = 'debug';
 clc
 
-% loading data without any specification
+% loading specified folder
+folder = "C:\Users\micha\OneDrive\Desktop\NRM\FOV1";
+
+%% only run_00000
+f1 = load_data(folder, 1);
+
+%% only run_00001
+f2 = load_data(folder, 2);
+
+%% all run files
+f3 = load_data(folder);
+
+%% loading data without any specification
 % should open dialog to select a folder with run_** files
 no_spec = load_data();
