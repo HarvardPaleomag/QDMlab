5b15b7641b6f8057c389a3bda72c96b836178901
Mike Volk <m.w.r.volk@uu.nl>
2022-05-30 16:21:12 +0200

    fix: better file handling

---
+++ io/read_header.m
@@ -0,0 +1,29 @@
+function headerFile = read_header(headerFile)
+%[header] = read_header(file)
+arguments 
+    headerFile = 'none'
+end
+
+headerFile = automatic_input_ui__(headerFile, 'type', 'file', 'single', true, 'title', 'select run_00*_header.txt file', 'filter', '*.txt');
+
+if ~isstruct(headerFile)
+    headerFile = dir(headerFile);
+end
+
+
+data = importdata(fullfile(headerFile.folder, headerFile.name));
+
+for i=1:size(data.rowheaders)
+    key = data.rowheaders{i};
+    key = strrep(key, ' ', '_');
+    key = strrep(key, '(', '');
+    key = strrep(key, ')', '');
+    key = strrep(key, '?', '');
+
+    headerFile.(key) = data.data(i);
+end
+
+msg = sprintf('File << %s >> in %s successfully loaded', headerFile.name, headerFile.folder);
+logMsg('debug',msg,1,0);
+
+end
