a8abaca46ef63dc960fb83a1c183b67ff4c2b2f7
Mike Volk <m.w.r.volk@uu.nl>
2022-06-24 14:22:19 +0200

    fix: where functions arent detected correctly

--- admin_tools/docstring.py
+++ admin_tools/docstring.py
@@ -1,31 +1,48 @@
 import os
 import admin_tools
 from admin_tools import arguments
+import numpy as np
 
-def get_docstring_indices(reload = False):
+
+def get_docstring_indices(reload=False, debug_fname=''):
     """
     Walks through all functions and gets the index for the docstrings
     """
-    code = admin_tools.preload_code(reload = reload)
-    params = arguments.get_in_out_params(reload = reload)
+    code = admin_tools.preload_code(reload=reload)
+    params = arguments.get_in_out_params(reload=reload)
     comment_block_index = {}
     for f in params:
         for package, files in code.items():
             for file, c in files.items():
-                strt_idx = [i for i, l in enumerate(c) if l.startswith('function') and f in l]
+                strt_idx = [i for i, l in enumerate(c) if l.startswith('function') and f' {f}(' in l.replace('=', ' ')]
                 if len(strt_idx) != 1:
                     # file does not include the function
                     continue
                 else:
                     strt_idx = strt_idx[0]
                 comment_idx = []
-                for i, l in enumerate(c[strt_idx+1:]):
-                    if '%' in l:
-                        comment_idx.append(i)
-                    else:
-                        break
+                idx = np.array([i for i, l in enumerate(c[strt_idx:]) if l.rstrip().startswith('%')])
+                df = np.diff(idx, prepend=True)
+
+                try:
+                    until_idx = np.where(df > 1)[0][0]
+                except IndexError:
+                    until_idx = len(idx)
+
+                comment_idx = idx[:until_idx]
+
                 comment_block_index[f] = comment_idx
-                break
+
+                if f == debug_fname:
+                    print(f, file)
+                    print(strt_idx, c[strt_idx])
+                    print('comment indices: ', comment_idx)
+                    print()
+                    print('extracted comments')
+                    print('------------------')
+                    for l in np.array(c)[comment_idx]:
+                        print(l, end='')
+
     return comment_block_index
 
 
@@ -40,10 +57,31 @@ def get_docstring(reload = False):
     for f, idx in comment_block_index.items():
         for package, files in code.items():
             for file, c in files.items():
-                strt_idx = [i for i, l in enumerate(c) if l.startswith('function') and f in l]
+                strt_idx = [i for i, l in enumerate(c) if l.startswith('function') and f' {f}(' in l.replace('=', ' ')]
                 if len(strt_idx) != 1:
                     # file does not include the function
                     continue
                 else:
                     docstring[f] = [c[i] for i in idx[1:]]
     return docstring
+
+
+def make_params_doc(p):
+    lines = ['', 'Parameters', '----------']
+    for i in p['parameters']:
+        lines.append(f'  {i}:')
+    for k, v in p['kwargs'].items():
+        lines.append(f'  {k}: ({v})')
+
+    lines.extend(['', 'Returns', '----------'])
+    for i in p['returns']:
+        lines.append(f'  {i}:')
+
+    lines = [f'% {i}' for i in lines]
+
+    for l in lines:
+        print(l)
+
+    # return lines
+    # return ''.join(lines)
+    # return '\n\r'.join(lines)
